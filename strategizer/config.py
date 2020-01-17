5c46d3ddd6ce3370ed37abef5d540ae2dd75cc23
Martin Albrecht <martinralbrecht@googlemail.com>
2020-01-17 15:56:53 +0000

    decode raw string

--- strategizer/config.py
+++ strategizer/config.py
@@ -17,7 +17,7 @@ cmds = [("git", "show", "-s", "--format=%cd", "HEAD", "--date=short"),
 
 for cmd in cmds:
     try:
-        r = str(subprocess.check_output(cmd).rstrip())
+        r = str(subprocess.check_output(cmd).rstrip().decode("ascii"))
         git_revision.append(r)
     except ValueError:
         pass
